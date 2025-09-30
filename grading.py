219b5692651ff4211a7ed0b0029652cf99f3151a
Abdul-abiola <abdulrahamanabdulrahaman03@gmail.com>
2025-09-30 16:31:01 -0700

    Add files via upload
    updated

---
+++ grading.py
@@ -0,0 +1,18 @@
+name = input("Type in your name: ")
+score = int(input("type in your score: "))
+if score >=  80 and score <= 100:
+    print(f" welcome {name} you got an A")
+elif score >= 70 and score <= 79:
+    print(f" {name} you got a B")
+elif score >= 60 and score <= 69:
+    print(f" {name} you got a C") 
+elif score >= 45 and score <= 59:
+    print(f" {name} you got a D")
+elif score >= 35 and score <= 44:
+    print(f" {name} you got an E")
+elif score >= 0 and score <=35:
+    print(f" {name} you got an F")
+else:
+    print(f" {name} error")
+
+https://github.com/Abdul-abiola/A-calculator/blob/main/calculator.py#     
